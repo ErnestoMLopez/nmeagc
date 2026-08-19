ebca32979f6442dc0bd9e3b288e8529b985bf782
ErnestoMLopez <ernesto.lopez@ing.unlp.edu.ar>
2026-08-19 01:50:58 +0000

    Reemplazo la inicialización de terminal de ratatui por una propia
    Esto permite por ejemplo habilitar la captura de mouse. Por ahora se mantiene la misma estructura de funciones asociadas y no se encapsula el comportamiento en ningún tipo de datos tipo Tui.

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,15 @@ pub mod app;
 pub mod event;
 pub mod gnss;
 pub mod nmea;
+pub mod terminal;
 pub mod theme;
 pub mod ui;
 pub mod widgets;
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
-    let terminal = ratatui::init();
+    let terminal = terminal::init()?;
     let result = App::new().run(terminal);
-    ratatui::restore();
+    terminal::reset()?;
     result
 }
---
+++ src/terminal.rs
@@ -0,0 +1,44 @@
+use color_eyre::Result;
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ratatui::{DefaultTerminal, Terminal, backend::CrosstermBackend};
+
+/// Initializes the terminal interface.
+///
+/// It enables the raw mode and sets terminal properties.
+pub fn init() -> Result<DefaultTerminal> {
+    set_panic_hook()?;
+
+    crossterm::terminal::enable_raw_mode()?;
+    crossterm::execute!(std::io::stdout(), EnterAlternateScreen, EnableMouseCapture)?;
+
+    let backend = CrosstermBackend::new(std::io::stdout());
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.hide_cursor()?;
+    terminal.clear()?;
+
+    Ok(terminal)
+}
+
+/// Resets the terminal interface.
+///
+/// This function is also used for the panic hook to revert the terminal properties if
+/// unexpected errors occur.
+pub fn reset() -> Result<()> {
+    crossterm::execute!(std::io::stdout(), DisableMouseCapture, LeaveAlternateScreen)?;
+    crossterm::terminal::disable_raw_mode()?;
+    Ok(())
+}
+
+/// Set panic hook to reset the terminal interface on panic.
+fn set_panic_hook() -> Result<()> {
+    let panic_hook = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |panic| {
+        reset().expect("failed to reset the terminal");
+        panic_hook(panic);
+    }));
+    Ok(())
+}

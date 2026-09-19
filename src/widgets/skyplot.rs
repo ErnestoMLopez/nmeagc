f96fe12c8f9614a746d4786d0c41cb1fdc545a37
ErnestoMLopez <ernesto.lopez@ing.unlp.edu.ar>
2026-09-19 01:23:40 -0300

    Fix skyplot labels

--- src/widgets/skyplot.rs
+++ src/widgets/skyplot.rs
@@ -170,9 +170,9 @@ impl<'a> Skyplot<'a> {
         ctx.print(1.0, 0.0, "E".green());
         ctx.print(0.0, -1.0, "S".green());
         ctx.print(-1.0, 0.0, "W".green());
-        ctx.print(0.9, -0.01, "90º".green());
-        ctx.print(0.6, -0.01, "60º".green());
-        ctx.print(0.3, -0.01, "30º".green());
+        ctx.print(0.9, -0.01, "0º".green());
+        ctx.print(0.6, -0.01, "30º".green());
+        ctx.print(0.3, -0.01, "60º".green());
     }
 
     fn draw_svs(ctx: &mut Context, svs: &[PlotableSv]) {
